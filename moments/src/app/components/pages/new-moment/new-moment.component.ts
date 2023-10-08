68394f42dcffb974c862ae188c4f6ac34af8172c
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2023-10-08 17:27:59 -0300

    add moments

---
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+
+  btnText = "Share"
+}
